cce4ed291159c47979d42b01902c83c741d0e762
FreshRec <114177791+FreshRec@users.noreply.github.com>
2025-11-26 22:03:38 +0300

    Update Auth.tsx

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -105,7 +105,7 @@ export default function Auth() {
       setMessage(null);
       try {
           const { error } = await supabase.auth.resetPasswordForEmail(email, {
-              redirectTo: window.location.href,
+              redirectTo: window.location.origin,
           });
           if (error) throw error;
           setMessage({ text: 'Ссылка для сброса пароля отправлена на ваш Email', type: 'success' });
