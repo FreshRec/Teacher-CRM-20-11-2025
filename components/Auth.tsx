1e594e90616c5c199e989e0412cae2039e794a5d
FreshRec <114177791+FreshRec@users.noreply.github.com>
2025-12-15 23:25:03 +0300

    Update Auth.tsx

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { api } from '../services/api';
 
 // Получаем URL для отображения (чтобы вы видели, куда идет запрос)
